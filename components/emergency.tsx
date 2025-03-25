0b1462139f192fa9dae148bf0888aa08933631f0
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-03-25 14:22:34 +0530

    Emergencies page completed

--- components/emergency.tsx
+++ components/emergency.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 
-const emergency = () => {
+export default function Emergency() {
   return (
-    <div>emergency</div>
-  )
+    <div className="max-w-5xl mx-auto px-8">
+        <span className="flex justify-center mt-8 text-3xl font-bold text-white">Emergency Contacts</span>
+      <HoverEffect items={projects} />
+    </div>
+  );
 }
-
-export default emergency
+export const projects = [
+  {
+    title: "Vineet Nijamkar",
+    description:
+      "Incoming Coordinator",
+    link: "tel:+91 9881132571",
+  },
+  {
+    title: "Ritam Goyal",
+    description:
+      "Incoming Coordinator",
+    link: "tel:+91 9711360035",
+  },
+  {
+    title: "KMC",
+    description:
+      "Hospital Ambulance",
+    link: "tel:+91 8202922761",
+  },
+  {
+    title: "Police",
+    description:
+      "Manipal Police Station",
+    link: "tel:+91 8202570328",
+  },
+];
