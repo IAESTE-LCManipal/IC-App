db1033967929280b9a7618fcc6451a03f1e9d385
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-04-07 10:56:36 +0530

    Quality of life Updates

--- app/emergencies/page.tsx
+++ app/emergencies/page.tsx
@@ -12,7 +12,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 
-export default function Emergency() {
+export default function Emergencies() {
     const { data: session, status } = useSession();
             const router = useRouter();
 
