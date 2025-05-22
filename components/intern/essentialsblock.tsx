5e6c064bee0ae906164e47777135c636fe3d3c44
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-05-22 13:16:03 +0530

    Update essentialsblock.tsx

--- components/intern/essentialsblock.tsx
+++ components/intern/essentialsblock.tsx
@@ -13,9 +13,9 @@ import Essentials from "@/components/intern/essential";
 
 export default function DialogDemo() {
     const [open, setOpen] = useState(false);
-    const [selectedItem, setSelectedItem] = useState<{ title: string; hidden: string } | null>(null);
+    const [selectedItem, setSelectedItem] = useState<{ title: string; description: string; hidden?: string } | null>(null);
 
-    const handleItemClick = (item: { title: string; hidden: string }) => {
+    const handleItemClick = (item: { title: string; description: string; hidden?: string }) => {
         setSelectedItem(item);
         setOpen(true); // Open the dialog when an item is clicked
     };
