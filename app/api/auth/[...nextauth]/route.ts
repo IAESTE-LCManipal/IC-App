eca16f91e33b29a106c2ad21e7dc22bfd0f40d6f
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2025-05-22 12:53:35 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,35 +10,35 @@ import { authOptions } from "../authOptions";
 
 // Define separate types for each user role
 
-type InternUser = {
-  id: string;
-  role: 'intern';
-  internID: string;
-  startDate: string;
-  endDate: string;
-  professorDetails: unknown;
-  name: string;
-  image: string;
-};
+// type InternUser = {
+//   id: string;
+//   role: 'intern';
+//   internID: string;
+//   startDate: string;
+//   endDate: string;
+//   professorDetails: unknown;
+//   name: string;
+//   image: string;
+// };
 
-type LCUser = {
-  id: string;
-  role: 'lc';
-  email: string;
-  firstName: string;
-  lastName: string;
-  sroSlot: string;
-};
+// type LCUser = {
+//   id: string;
+//   role: 'lc';
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+//   sroSlot: string;
+// };
 
-type AdminUser = {
-  id: string;
-  role: 'admin';
-  email: string;
-  firstName: string;
-  lastName: string;
-};
+// type AdminUser = {
+//   id: string;
+//   role: 'admin';
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+// };
 
-type UserWithRole = InternUser | LCUser | AdminUser;
+// type UserWithRole = InternUser | LCUser | AdminUser;
 
 // Only export the handler(s)
 const handler = NextAuth(authOptions);
